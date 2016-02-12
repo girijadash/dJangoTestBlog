42b21750bb4dd86becba7c8f1b2528878567e613
Girija Dash <girija.dash@in.g4s.com>
2016-02-12 12:42:35 +0530

    Removed Importing MongoEngine

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import mongoengine # Added By Girija 
+#import mongoengine # Added By Girija 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
